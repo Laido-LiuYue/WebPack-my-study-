0d38f69547ffc6629057eb2f3a20cd8aa737afd0
Laido <578201842@qq.com>
2019-04-18 11:12:20 +0800

    font

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from'./containers/Home';
 // import 'antd-mobile/dist/antd-mobile.css';
 import './index.less';
+import './assets/common.less';
+import Home from'./containers/Home';
 // let ele = <h1>hello</h1>;
 ReactDOM.render(
     <Home />,
